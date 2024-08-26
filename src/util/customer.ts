4be9229f2e0e8f4157d8bc2f312bd0bfcc7f5ae6
ttswap <ttswap.exchange@gmail.com>
2024-08-26 23:59:27 +0800

    add customer.ts 20240827
    Signed-off-by: ttswap <ttswap.exchange@gmail.com>

---
+++ src/util/customer.ts
@@ -0,0 +1,33 @@
+import { Customer, CustomerData } from "../../generated/schema";
+
+import { BigInt } from "@graphprotocol/graph-ts";
+
+export function log_CustomerData(
+        customer: Customer,
+        modifiedTime: BigInt
+): void {
+        let data_week = modifiedTime.mod(BigInt.fromU32(604800));
+        let customerData_week = CustomerData.load(
+                customer.id + "w" + data_week.toString()
+        );
+        if (customerData_week === null) {
+                customerData_week = new CustomerData(
+                        customer.id + "w" + data_week.toString()
+                );
+                customerData_week.refer = customer.refer;
+                customerData_week.tradeValue = customer.tradeValue;
+                customerData_week.investValue = customer.investValue;
+                customerData_week.disinvestValue = customer.disinvestValue;
+                customerData_week.tradeCount = customer.tradeCount;
+                customerData_week.investCount = customer.investCount;
+                customerData_week.disinvestCount = customer.disinvestCount;
+                customerData_week.isBanlist = customer.isBanlist;
+                customerData_week.customerno = customer.customerno;
+                customerData_week.totalprofitvalue = customer.totalprofitvalue;
+                customerData_week.totalcommissionvalue =
+                        customer.totalcommissionvalue;
+                customerData_week.customerid = customer.id;
+                customerData_week.create_time = modifiedTime;
+                customerData_week.save();
+        }
+}
